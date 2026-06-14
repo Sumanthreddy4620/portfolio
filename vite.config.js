d07742ae460f2ce2149eee66ad95a8bbf08d8226
Sumanth Reddy <k.sumanthreddy4620@gmail.com>
2026-06-15 00:06:48 +0530

    fix build - remove sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      hostname: 'https://sumanthreddykasireddy.vercel.app'
-    })
   ],
   resolve: {
     alias: {
